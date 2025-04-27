5ed58f2cfc8b9841619e054bf2dd4918c8f55499
Manuel Louie Gayao <louiemanuel238@gmail.com>
2025-04-27 11:32:22 -0600

    add redundancy , API call to get user

--- src/components/InfoHeader/InfoHeader.vue
+++ src/components/InfoHeader/InfoHeader.vue
@@ -1,13 +1,32 @@
 <script setup>
-import { ref } from 'vue';
-import data from '../../data/data.json';
+import JSONdata from '../../data/data.json';
+import { ref, watchEffect } from 'vue';
+import axios from 'axios';
+
+const BASE_URL = import.meta.env.VITE_SERVER_BASE_URL;
+const PORT = import.meta.env.VITE_SERVER_PORT;
+const data = ref();
+
+watchEffect(()=>{
+  const getUser = async ()=>{
+    try {
+      const response = await axios.get(`${BASE_URL}:${PORT}/user/1`);
+      data.value = response.data;
+    }catch(error){
+
+    }
+  }
+  getUser()
+})
+
+
 </script>
 
 <template>
 <div class="personal-info">
-  <h1 class="personal-info__name">{{ data.name }}</h1>
-  <h2 class="personal-info__title text-title">{{ data.title }}</h2>
-  <p class="personal-info__description text-description">{{ data.description }}</p>
+  <h1 class="personal-info__name">{{`${data?.first_name} ${data?.last_name}` || JSONdata.name}}</h1>
+  <h2 class="personal-info__title text-title">{{  data?.title || JSONdata.title}}</h2>
+  <p class="personal-info__description text-description">{{  data?.tagline || JSONdata.tagline}}</p>
 </div>
 </template>
 
