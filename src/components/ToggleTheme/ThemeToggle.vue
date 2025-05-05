74c938ce6d1c7fa11fb7f41d4fdfc8ff77f0f8bb
Manuel Louie Gayao <louiemanuel238@gmail.com>
2025-05-05 15:27:02 -0600

    add filter toggle button for theme

--- src/components/ToggleTheme/ThemeToggle.vue
+++ src/components/ToggleTheme/ThemeToggle.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { useDark, useToggle, useColorMode } from '@vueuse/core'
-
-const isDark = useDark()
-const toggleDark = useToggle(isDark)
+import {ref} from 'vue';
+import { useToggle, useColorMode } from '@vueuse/core'
+const isOpenToggle = ref(false)
+const openToggle = useToggle(isOpenToggle);
 const colorMode = useColorMode({
   modes:{
-    dim: 'dim',
+    dim: 'contrast',
     cafe: 'cafe',
   },
 });
@@ -14,13 +14,19 @@ const colorMode = useColorMode({
 <template>
   
   <div class="theme-toggle">
-    <div>
-    <button>Toggle</button>
-  </div>
-  <button @click="colorMode = 'dark'"><span>Dark</span></button>
-  <button @click="colorMode = 'light'"><span>Light</span></button>
-  <button @click="colorMode = 'dim'"><span>Dim</span></button>
-  <button @click="colorMode = 'cafe'"><span>Cafe</span></button>
+    <button @click="openToggle()" class="theme-toggle__button">
+    <img v-if="colorMode === 'dark'" src="/icons/dark-toggle.svg" alt="Dark Mode Icon" class="theme-toggle__img" />
+    <img v-if="colorMode === 'light'" src="/icons/light-toggle.svg" alt="Light Mode Icon" class="theme-toggle__img" />
+    <img v-if="colorMode === 'cafe'" src="/icons/cafe-toggle.svg" alt="Cafe Mode Icon" class="theme-toggle__img" />
+    <img v-if="colorMode === 'contrast'" src="/icons/contrast-toggle.svg" alt="Contrast Mode Icon" class="theme-toggle__img" />
+    <span class="theme-toggle__button--text">{{ colorMode.toUpperCase() }}</span>
+    </button>
+    <div v-if="isOpenToggle" class="theme-toggle__options">
+      <button @click="colorMode = 'dark'; openToggle()"><span>Dark</span></button>
+      <button @click="colorMode = 'light'; openToggle()"><span>Light</span></button>
+      <button @click="colorMode = 'contrast'; openToggle()"><span>Contrast</span></button>
+      <button @click="colorMode = 'cafe'; openToggle()"><span>Cafe</span></button>
+    </div>
   </div>
 </template>
 
@@ -29,56 +35,58 @@ const colorMode = useColorMode({
 @use '../../styles/partials/mixins' as *;
 .theme-toggle{
   position: absolute;
-  top: 0;
-  right: 0;
+  top: 1rem;
+  right: 1rem;
+  display: flex;
+  background-color: #5f6c79;
+  border-radius: 15px;
+  height: 30px;
+  
+  &__button{
+    display: flex;
+    align-items: center;
+    font-weight: 600;
+    background-color: #435364;
+    border-radius: 10px;
+    padding: 0 1rem;
+    &--text{
+      display: none;
+      @include large-mobile{
+        display: initial;
+      }
+    }
+  }
+  &__options{
+    display: flex;
+    align-items: center;
+  }
 }
 .dark{
   color: $text-dark-main-color;
   background-color: $background-dark-main-color;
 }
 .light{
-  color: $text-light-main-color;
-  background-color:  $background-light-main-color;
-  .tech-list__tag {
-    color: $text-light-tag-color;
-    background-color: $background-light-tag-color;
+ @include lightTheme;
+ .theme-toggle {
+  background-color: #5f6c79;
+  &__button{
+    background-color: #b7bdc4;
   }
-  .text-description, .profile-text{
-    color: $text-light-sub-color;
-  }
-  .project-card, .work-card{
-    &:hover{
-      color: $text-light-tag-color;
-    }
-  }
-  .contact{
-    &__input{
-      background-color: $background-light-input-color;
-    }
-  }
-  .work-date{
-    color: $text-light-sub-color;
-  }
-  .button-regular{
-    background-color: #3b3939;
-  }
-  .focused{
-    color: $text-light-tag-color;
-    font-weight: 600;
-  }
-  .contact{
-    &__input{
-      color: $text-light-main-color;
-    }
-  }
+ }
 }
-.dim{
-  color: #ffffffde;
-  background-color: #0E1D35;
+.contrast{
+  filter: contrast(1.2);
 }
 .cafe{
-  color: #ffffffde;
-  background-color: #b9b79d;
+  @include lightTheme;
+  filter: hue-rotate(315deg) brightness(0.9);
+  filter: sepia(20%);
+  .theme-toggle {
+  background-color: #5f6c79;
+  &__button{
+    background-color: #b7bdc4;
+  }
+ }
 }
 
 </style>
