abb8c22aee5259e8c5c562ffba2df1dbe91442b5
Manuel Louie Gayao <louiemanuel238@gmail.com>
2025-05-14 12:32:54 -0600

    change toggle icons; add SEO

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,20 @@
 <script setup>
 import NavigationSection from './NavigationSection.vue';
 import MainSection from './MainSection.vue';
+import { useHead, useSeoMeta } from '@unhead/vue'
+useHead({
+  title: 'Web Developer',
+  titleTemplate: '%s %separator %siteName',
+  templateParams: {
+    siteName: 'Louie Gayao'
+  }
+});
+useSeoMeta({
+  description: 'Louie Gayao Home Page, Web Developer, Full-Stack Developer',
+  ogTitle: '%s', 
+});
+
+
 </script>
 <template>
     <NavigationSection/>
