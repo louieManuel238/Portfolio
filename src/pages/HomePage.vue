9b0a202b511f3b86e65d6e5c2133101d9d3ab09b
Manuel Louie Gayao <louiemanuel238@gmail.com>
2025-05-17 16:41:59 -0600

    add analytics

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,28 @@
 <script setup>
 import NavigationSection from './NavigationSection.vue';
 import MainSection from './MainSection.vue';
-import { useHead, useSeoMeta } from '@unhead/vue'
+import { useHead, useSeoMeta } from '@unhead/vue';
+
 useHead({
   title: 'Web Developer',
   titleTemplate: '%s %separator %siteName',
   templateParams: {
     siteName: 'Louie Gayao'
-  }
+  },
+  script: [
+    {
+      src: "https://www.googletagmanager.com/gtag/js?id=G-R4YCPQ0027",
+      async: true
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-R4YCPQ0027');
+      `
+    }
+  ]
 });
 useSeoMeta({
   description: 'Louie Gayao Home Page, Web Developer, Full-Stack Developer',
