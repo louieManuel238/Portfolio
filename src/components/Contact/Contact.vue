32271a9906bb76ddc6f1f186149e9f2f9e1b93e3
Manuel Louie Gayao <louiemanuel238@gmail.com>
2025-05-16 20:00:30 -0600

    add toaster with styleing on submit

--- src/components/Contact/Contact.vue
+++ src/components/Contact/Contact.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Socials from '../Socials/Socials.vue';
+import Toast from '../Modal/Toast.vue';
 import {ref, inject, watch, useTemplateRef} from 'vue';
 import { useFocus, useWindowScroll } from '@vueuse/core';
 import emailjs from '@emailjs/browser';
@@ -10,7 +11,7 @@ const inputMessage = useTemplateRef('inputMessage')
 const { focused: isFocusedName } = useFocus(inputName);
 const { focused: isFocusedEmail } = useFocus( inputEmail);
 const { focused: isFocusedMessage } = useFocus(inputMessage);
-
+const isError = ref(false);
 
 const visibleSection = inject('visibleSection')
 const {y} = useWindowScroll();
@@ -27,23 +28,35 @@ const form = ref({
 const sendStatus = ref(false);
 
 const sendEmail = () => {
-  emailjs
-    .send(import.meta.env.VITE_EMAIL_SERVICE, import.meta.env.VITE_EMAIL_TEMPLATE, 
-    {   name: form.value.name,
-        message: form.value.message,
-        email: form.value.email,
-    },
-    {
-      publicKey: import.meta.env.VITE_EMAIL_KEY,
-    })
-    .then(
-      () => {
-       sendStatus = true;
-      },
-      (error) => {
-        sendStatus = false;
-      },
-    );
+    isError.value = false;
+    const {name, message, email} = form.value;
+    if(name != ''
+    &&message != ''
+    &&email != ''){
+       
+    emailjs
+        .send(import.meta.env.VITE_EMAIL_SERVICE, import.meta.env.VITE_EMAIL_TEMPLATE, 
+        {   name: form.value.name,
+            message: form.value.message,
+            email: form.value.email,
+        },
+        {
+        publicKey: import.meta.env.VITE_EMAIL_KEY,
+        })
+        .then(
+        () => {
+             sendStatus.value = true;
+            setTimeout(()=> sendStatus.value = false, 3000)
+        },
+        (error) => {
+            sendStatus.value = false;
+            isError.value = true;
+        },
+        );
+    }else{
+       isError.value = true;
+    }
+    
 };
 </script>
 <template>
@@ -55,26 +68,33 @@ const sendEmail = () => {
         <form class="contact" @submit.prevent="sendEmail">
             <label class="contact__label" for="full-name" :class="{focused: isFocusedName}">Full Name</label>
             <input ref="inputName" class="contact__input" aria-label="input name" autocomplete="on"
-            type="text" id="full-name" name="name" v-model="form.name">
+            type="text" id="full-name" name="name" v-model="form.name" :class="{error:isError}">
 
             <label class="contact__label" for="email-address" :class="{focused: isFocusedEmail}">Email</label>
             <input ref="inputEmail" class="contact__input" aria-label="input email address" autocomplete="on"
-            type="email" id="email-address" name="email" v-model="form.email" >
+            type="email" id="email-address" name="email" v-model="form.email" :class="{error:isError}">
 
             <label class="contact__label" for="message" :class="{focused: isFocusedMessage}">Message</label>
             <textarea ref="inputMessage" aria-label="input massage" 
-            class="contact__input contact__input--textarea" id="message" name="message" v-model="form.message"></textarea>
+            class="contact__input contact__input--textarea" 
+            id="message" name="message" v-model="form.message" :class="{error:isError}"></textarea>
             <button type="submit" class="button-regular" aria-label="submit message button">Send</button>
         </form>
     </section>
+    <Transition name="toast">
+        <Toast v-if="sendStatus" ><p v-if="sendStatus">Message Sent!</p></Toast>
+    </Transition>
 </template>
 <style lang="scss" scoped>
 @use '../../styles/partials/variables' as *;
+.error{
+    border: 1px solid #ff0000;
+}
 .contact{
     display: flex;
     flex-direction: column;
 
-    padding: 0 1rem;
+    padding: 0 1rem 2rem;
     &__label{
         width: 100%;
         margin-top: .75rem;
@@ -111,5 +131,18 @@ const sendEmail = () => {
     color: #00C2CB;
     font-weight: 600;
 }
-
+.toast-enter-from,
+.toast-leave-to{
+    opacity: 0;
+    transform: translateY(60px);
+}
+.toast-enter-to,
+.toast-leave-from{
+    opacity: 1;
+    transform: translateY(0);
+}
+.toast-enter-active,
+.toast-leave-active{
+    transition: all 0.3s ease;
+}
 </style>
---
+++ src/components/Modal/Toast.vue
@@ -0,0 +1,31 @@
+<script setup>
+const { message } = defineProps({
+    message: String
+});
+</script>
+<template>
+    <div class="toast">
+        <p class="toast__message"><slot>Error</slot></p>
+    </div>
+</template>
+
+<style lang="scss">
+.toast{
+    width: 400px;
+    height: 50px;
+    display: flex;
+    position: fixed;
+    bottom: 40px;
+    right: 0;
+    left: 0;
+    margin-left: auto;
+    margin-right: auto;
+    justify-content: center;
+    align-items: center;
+    border-radius: 1.25rem;
+    background-color: #9bfd6d50;
+    color: #328b02;
+    font-size: 24px;
+
+}
+</style>
